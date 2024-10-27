514e1a4ac9753eb8e4325f1710fb5ce15ad01714
ShahriarCode <shahriarcode69@gmail.com>
2024-10-27 19:46:16 +0600

    full website done

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+// vite.config.js
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+        services: resolve(__dirname, 'service.html'),
+        contact: resolve(__dirname, 'contact.html'),
+        faq: resolve(__dirname, 'faq.html'),
+      },
+    },
+  },
+})
